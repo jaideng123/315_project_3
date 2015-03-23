44d5e66fd9815d26070ac153d2c4f4c863a16a8f
Jaiden Gerig <jaideng123@yahoo.com>
2015-03-23 12:46:47 -0500

    Added explantation of circuit simulator

--- Simulator.java
+++ Simulator.java
@@ -1,5 +1,11 @@
 /**
  * Created by Jaiden on 3/23/2015.
+ * This class will be initialized with a desired truth table
+ * then fed a circuit to evalulate
+ * it will test all combinations of input and return an integer
+ * the integer represents how many lines of the truth table the best output
+ * of the circuit lined up with
+ * a 0 will be returned if a circuit contains too many or too few inputs
  */
 public class Simulator {
 }
