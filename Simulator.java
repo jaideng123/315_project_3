37658584d7f459ff50f26bb9841415b4d8d2bbb4
jaideng123 <jaideng123@yahoo.com>
2015-03-24 10:33:59 -0500

    Created Constructor for simulator

--- Simulator.java
+++ Simulator.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 /**
  * Created by Jaiden on 3/23/2015.
  * This class will be initialized with a desired truth table
@@ -7,5 +9,17 @@
  * of the circuit lined up with
  * a 0 will be returned if a circuit contains too many or too few inputs
  */
+
 public class Simulator {
+    boolean[][] inputs;
+    boolean[][] outputs;
+    //desired inputs and corresponding outputs for truth table
+    public Simulator(boolean[][] in,boolean[][] out ){
+        if(in.length != out.length){
+            throw new IllegalArgumentException("Truth table inputs and outputs do not match");
+        }
+        inputs = in;
+        outputs = out;
+    }
+
 }
