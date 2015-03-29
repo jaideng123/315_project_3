d7de6e90526dee3eda58ff9575ceaf538c0d7658
sjroh <sijine@gmail.com>
2015-03-29 17:21:48 -0500

    Update Node.java

--- logicga/Node.java
+++ logicga/Node.java
@@ -9,35 +9,20 @@ import java.util.Vector;
 
 public class Node {
 	
-	int nnot = 0; // number of not gates
 	Circuit circuit = new Circuit();
 	int ninput;
 	int noutput;
-
+	boolean blocked = false; // if it's true, there are 2 NOT gates
+	
 	Vector<Integer> inputlines = new Vector();
 	Vector<Integer> outputlines = new Vector();
 	
-//	Node () {
-//		
-//	}
 	
 	Node (Circuit c) {
 		circuit = c;
 		refresh();
-//		inputlines = c.getInputLines();
-//		outputlines = c.getOutputLines();
-//		ninput = inputlines.size();
-//		noutput = outputlines.size();
-		// can get number of inputs from Circuit class?
-		// can get number of outputs from Circuit class?
 	}
-	
-//	Node (Circuit c, int i, int o) {
-//		circuit = c;
-//		ninput = i;
-//		noutput = o;
-//	}
-	
+		
 	public void refresh() {
 		inputlines = circuit.getInputLines();
 		outputlines = circuit.getOutputLines();
@@ -45,19 +30,18 @@ public class Node {
 		noutput = outputlines.size();
 	}
 	
-//	public void addGate(int output, String gateType, Vector input){
-//    	circuit.genes.add(new Gene(output, gateType, input));
-//    	refresh();
-//    	if (gateType.endsWith("Not")) {
-//    		nnot ++;
-//    	}
-//    }
-	
-	public boolean canUseNot(){
-		if (nnot >= 2) {
-			return false;
-		} else {
-			return true;
-		}
-	} 
+    public void addGate(int output, String gateType, Vector input){
+    	blocked = !circuit.addGate(output, gateType, input);
+    }
+
+    public Vector<Integer> getOutputlines(){
+    	return circuit.getOutputLines();
+    }
+    public Vector<Integer> getIntputlines(){
+    	return circuit.getInputLines();
+    }
+    
+    public int getNots() {
+    	return circuit.n_not;
+    }
 }
