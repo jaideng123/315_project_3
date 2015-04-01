f4557a293499429f05f4ccc8d0be15727c692d51
jaideng123 <jaideng123@yahoo.com>
2015-04-01 13:16:24 -0500

    Added initial Population function

--- logicga/LogicGA.java
+++ logicga/LogicGA.java
@@ -2,8 +2,10 @@
 package logicga;
 
 import java.io.IOException;
+import java.security.PublicKey;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Vector;
 import java.lang.Math;
 
@@ -17,7 +19,212 @@ public class LogicGA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+        System.out.println("Breadth First Search START");
+
+        // real full adder inputs
+        boolean[][] fulladder_inputs = new boolean[8][3];
+        fulladder_inputs[0][0] = false;
+        fulladder_inputs[0][1] = false;
+        fulladder_inputs[0][2] = false;
+        fulladder_inputs[1][0] = false;
+        fulladder_inputs[1][1] = false;
+        fulladder_inputs[1][2] = true;
+        fulladder_inputs[2][0] = false;
+        fulladder_inputs[2][1] = true;
+        fulladder_inputs[2][2] = false;
+        fulladder_inputs[3][0] = false;
+        fulladder_inputs[3][1] = true;
+        fulladder_inputs[3][2] = true;
+        fulladder_inputs[4][0] = true;
+        fulladder_inputs[4][1] = false;
+        fulladder_inputs[4][2] = false;
+        fulladder_inputs[5][0] = true;
+        fulladder_inputs[5][1] = false;
+        fulladder_inputs[5][2] = true;
+        fulladder_inputs[6][0] = true;
+        fulladder_inputs[6][1] = true;
+        fulladder_inputs[6][2] = false;
+        fulladder_inputs[7][0] = true;
+        fulladder_inputs[7][1] = true;
+        fulladder_inputs[7][2] = true;
+
+        boolean[][] fulladder_outputs = new boolean[8][2];
+        fulladder_outputs[0][0] = false;
+        fulladder_outputs[0][1] = false;
+        fulladder_outputs[1][0] = false;
+        fulladder_outputs[1][1] = true;
+        fulladder_outputs[2][0] = false;
+        fulladder_outputs[2][1] = true;
+        fulladder_outputs[3][0] = true;
+        fulladder_outputs[3][1] = false;
+        fulladder_outputs[4][0] = false;
+        fulladder_outputs[4][1] = true;
+        fulladder_outputs[5][0] = true;
+        fulladder_outputs[5][1] = false;
+        fulladder_outputs[6][0] = true;
+        fulladder_outputs[6][1] = false;
+        fulladder_outputs[7][0] = true;
+        fulladder_outputs[7][1] = true;
+
+        // inverter
+        boolean[][] inverse_inputs = new boolean[8][3];
+        inverse_inputs[0][0] = false;
+        inverse_inputs[0][1] = false;
+        inverse_inputs[0][2] = false;
+        inverse_inputs[1][0] = false;
+        inverse_inputs[1][1] = false;
+        inverse_inputs[1][2] = true;
+        inverse_inputs[2][0] = false;
+        inverse_inputs[2][1] = true;
+        inverse_inputs[2][2] = false;
+        inverse_inputs[3][0] = false;
+        inverse_inputs[3][1] = true;
+        inverse_inputs[3][2] = true;
+        inverse_inputs[4][0] = true;
+        inverse_inputs[4][1] = false;
+        inverse_inputs[4][2] = false;
+        inverse_inputs[5][0] = true;
+        inverse_inputs[5][1] = false;
+        inverse_inputs[5][2] = true;
+        inverse_inputs[6][0] = true;
+        inverse_inputs[6][1] = true;
+        inverse_inputs[6][2] = false;
+        inverse_inputs[7][0] = true;
+        inverse_inputs[7][1] = true;
+        inverse_inputs[7][2] = true;
+
+        boolean[][] inverse_outputs = new boolean[8][3];
+        for (int i = 0; i < 8; i ++) {
+            for (int j = 0; j < 3; j ++) {
+                inverse_outputs[i][j] = !inverse_inputs[i][j];
+            }
+        }
+
+        // one AND circuit
+        boolean[][] and_inputs = new boolean [4][2];
+        and_inputs[0][0] = false;
+        and_inputs[0][1] = false;
+        and_inputs[1][0] = false;
+        and_inputs[1][1] = true;
+        and_inputs[2][0] = true;
+        and_inputs[2][1] = false;
+        and_inputs[3][0] = true;
+        and_inputs[3][1] = true;
+
+        boolean[][] and_outputs = new boolean [4][1];
+        and_outputs[0][0] = false;
+        and_outputs[1][0] = false;
+        and_outputs[2][0] = false;
+        and_outputs[3][0] = true;
+
+        // one OR circuit
+        boolean[][] or_inputs = new boolean [4][2];
+        or_inputs[0][0] = false;
+        or_inputs[0][1] = false;
+        or_inputs[1][0] = false;
+        or_inputs[1][1] = true;
+        or_inputs[2][0] = true;
+        or_inputs[2][1] = false;
+        or_inputs[3][0] = true;
+        or_inputs[3][1] = true;
+
+        boolean[][] or_outputs = new boolean [4][1];
+        or_outputs[0][0] = false;
+        or_outputs[1][0] = true;
+        or_outputs[2][0] = true;
+        or_outputs[3][0] = true;
+
+        // one NAND circuit
+        boolean[][] nand_inputs = new boolean [4][2];
+        nand_inputs[0][0] = false;
+        nand_inputs[0][1] = false;
+        nand_inputs[1][0] = false;
+        nand_inputs[1][1] = true;
+        nand_inputs[2][0] = true;
+        nand_inputs[2][1] = false;
+        nand_inputs[3][0] = true;
+        nand_inputs[3][1] = true;
+
+        boolean[][] nand_outputs = new boolean [4][1];
+        nand_outputs[0][0] = true;
+        nand_outputs[1][0] = true;
+        nand_outputs[2][0] = true;
+        nand_outputs[3][0] = false;
+
+        // one OR circuit
+        boolean[][] nor_inputs = new boolean [4][2];
+        nor_inputs[0][0] = false;
+        nor_inputs[0][1] = false;
+        nor_inputs[1][0] = false;
+        nor_inputs[1][1] = true;
+        nor_inputs[2][0] = true;
+        nor_inputs[2][1] = false;
+        nor_inputs[3][0] = true;
+        nor_inputs[3][1] = true;
+
+        boolean[][] nor_outputs = new boolean [4][1];
+        nor_outputs[0][0] = true;
+        nor_outputs[1][0] = false;
+        nor_outputs[2][0] = false;
+        nor_outputs[3][0] = false;
+
+
+        // one XOR circuit
+        boolean[][] custom_inputs = new boolean [4][2];
+        custom_inputs[0][0] = false;
+        custom_inputs[0][1] = false;
+        custom_inputs[1][0] = false;
+        custom_inputs[1][1] = true;
+        custom_inputs[2][0] = true;
+        custom_inputs[2][1] = false;
+        custom_inputs[3][0] = true;
+        custom_inputs[3][1] = true;
+
+        boolean[][] custom_outputs = new boolean [4][1];
+        custom_outputs[0][0] = true;
+        custom_outputs[1][0] = false;
+        custom_outputs[2][0] = true;
+        custom_outputs[3][0] = true;
+
+        // menu
+        System.out.println("1. Full adder circuit");
+        System.out.println("2. Inverse circuit");
+        System.out.println("3. one AND circuit");
+        System.out.println("4. one OR circuit");
+        System.out.println("5. one NAND circuit");
+        System.out.println("6. one NOR circuit");
+        System.out.println("7. one XOR circuit");
+
+        System.out.print("Choose one: ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        Simulator sim = null;
+
+        switch(num) {
+            case 1:
+                sim = new Simulator(fulladder_inputs, fulladder_outputs);
+                break;
+            case 2:
+                sim = new Simulator(inverse_inputs, inverse_outputs);
+                break;
+            case 3:
+                sim = new Simulator(and_inputs, and_outputs);
+                break;
+            case 4:
+                sim = new Simulator(or_inputs, or_outputs);
+                break;
+            case 5:
+                sim = new Simulator(nand_inputs, nand_outputs);
+                break;
+            case 6:
+                sim = new Simulator(nor_inputs, nor_outputs);
+                break;
+            case 7:
+                sim = new Simulator(custom_inputs, custom_outputs);
+                break;
+        }
+        Population p = null;
 
     }
     //returns 2 offspring
@@ -62,5 +269,46 @@ public class LogicGA {
 
         return randomNum;
     }
+    public static Population initialPop(int inputs, int size, Simulator s){
+        Population initial = new Population();
+        for (int j = 0; j < size; j++) {
+            //set up base for circuit
+            Circuit c = new Circuit();
+            for (int i = 0; i < inputs; i++) {
+                Vector<Integer> in = new Vector<Integer>();
+                in.addElement(i);
+                c.genes.add(new Gene(i, "None", in));
+            }
+            int r = randInt(1,inputs*5);
+            for (int i = 0; i < r; i++) {
+                c.genes.add(randomGate(c));
+            }
+            c.calculateFitness(s);
+            initial.add(c);
+        }
+        return initial;
+    }
+    public static Gene randomGate(Circuit c){
+        int r = randInt(1,10);
+        Vector<Integer> in = new Vector<Integer>();
+        Gene g = new Gene(c.genes.size()+1,"None",in);
+        if(r <= 4){
+            g.type = "And";
+            g.inputs.add(randInt(1,c.genes.size()));
+            g.inputs.add(randInt(1, c.genes.size()));
+        }
+        else if(r <= 8){
+            g.type = "Or";
+            g.inputs.add(randInt(1,c.genes.size()));
+            g.inputs.add(randInt(1, c.genes.size()));
+        }
+        else if(r <= 10){
+            g.type = "Not";
+            g.inputs.add(randInt(1,c.genes.size()));
+        }
+        return g;
+    }
+
+    //public
     
 }
