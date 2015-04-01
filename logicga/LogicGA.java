dd3f039b97947f20ec3a3a193918e56c43886776
jaideng123 <jaideng123@yahoo.com>
2015-04-01 12:19:08 -0500

    Cleaned Test code out of logicGa

--- logicga/LogicGA.java
+++ logicga/LogicGA.java
@@ -17,48 +17,7 @@ public class LogicGA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //tests for simulator
-        Circuit test = new Circuit();
-        Integer arr[] = new Integer[]{1};
-        test.genes.add(new Gene(1,"NONE",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2};
-        test.genes.add(new Gene(2,"NONE",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{1};
-        test.genes.add(new Gene(3,"NOT",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2};
-        test.genes.add(new Gene(4,"NOT",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{1,4};
-        test.genes.add(new Gene(5,"AND",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2,3};
-        test.genes.add(new Gene(6,"AND",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{5,6};
-        test.genes.add(new Gene(7,"OR",new Vector(Arrays.asList(arr))));
-        Circuit test2 = new Circuit();
-        arr = new Integer[]{1};
-        test2.genes.add(new Gene(1,"NONE",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2};
-        test2.genes.add(new Gene(2,"NONE",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{1,2};
-        test2.genes.add(new Gene(3,"AND",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2,1};
-        test2.genes.add(new Gene(4,"OR",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{1,4};
-        test2.genes.add(new Gene(5,"AND",new Vector(Arrays.asList(arr))));
-        arr = new Integer[]{2,3};
-        test2.genes.add(new Gene(6,"OR",new Vector(Arrays.asList(arr))));
-       // Simulator s = new Simulator(new boolean[][]{{false,false},{false,true},{true,false},{true,true}},
-        //        new boolean[][]{{false},{true},{true},{false}});
-        //System.out.println(s.simulate(test));
-       /* try{
-        test.getFromFile(1);
-        }
-        catch(IOException e){
-            System.out.println(e.getMessage());
-        }*/
-        Circuit children[] = reproduce(test, test2);
-        children[0].Print();
-        System.out.println("");
-        children[1].Print();
+      
 
     }
     //returns 2 offspring
