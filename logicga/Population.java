39d69fdb03dd1b4e757b790ee2caac3ba9b3b1b6
mankusn <mankusnick@yahoo.com>
2015-04-04 00:07:39 -0500

    Update Population.java

--- logicga/Population.java
+++ logicga/Population.java
@@ -1,5 +1,6 @@
 package logicga;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Vector;
 
@@ -25,6 +26,46 @@ public class Population {
     }
     public Circuit getTopCircuit(){return population.poll();}
     
+    public int highestFitness(){
+    	return population.peek().fitness;
+    }
+    public int lowestFitness(){
+    	Circuit [] newArray = new Circuit[population.size()];
+    	population.toArray(newArray);
+    	Arrays.sort(newArray);
+    	return newArray[newArray.length-1].fitness;
+    }
+    //determines quality of population
+    public double getQuality(){
+    	int fmax = highestFitness();
+    	int fmin = lowestFitness();
+    	
+    	double q = fmin/(Math.sqrt(Math.pow(fmax, 2)+Math.pow(fmin, 2)));
+    	
+    	return q;
+    	
+    }
+    public double getStdev(){
+    	Circuit [] newArray = new Circuit[population.size()];
+    	population.toArray(newArray);
+    	double mean = Mean(newArray);
+    	double s = 0;
+    	for(int i = 0; i < newArray.length; i++){
+    		   s = Math.pow((newArray[i].fitness-mean),2);
+    		}
+    	return  Math.sqrt(s/(newArray.length -1));
+    	
+    }
+    public double Mean(Circuit[] a){
+    	
+    	double m = 0.0;
+    	long sum = 0;
+    	for(int i = 0;i<a.length;i++){
+    		sum += a[i].fitness;
+    	}
+    	m = sum/a.length;
+    	return m;
+    }
     public void add(Circuit a){
     	population.add(a);
     }
